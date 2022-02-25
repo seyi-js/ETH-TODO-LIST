05b6366d8eaf901ea849378839bb12327ae017a8
SAMUEL ADEBAYO OLUWASEYI <adebayosamueljahsmine30@gmail.com>
2022-02-25 20:34:46 +0100

    .

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -4,4 +4,22 @@ pragma solidity >=0.4.22 <0.9.0;
 contract TodoList {
     //Number of task
     uint256 public taskCount = 0;
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint256 => Task) public tasks;
+
+    constructor() public {
+        createTask("Wash clothe");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
